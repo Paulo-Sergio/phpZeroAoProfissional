d32ef71da5658726d5a8ef45ac22f9bbfa356e58
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-03-06 22:11:59 -0300

    iniciando estudos do slimFrameworks

---
+++ slimFrameworks/src/classes/Lista.php
@@ -0,0 +1,52 @@
+<?php
+
+class Lista {
+
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function getLista() {
+        $result = array();
+
+        $stmt = $this->db->prepare("SELECT * FROM lista");
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+            $result = $stmt->fetchAll();
+        }
+        return $result;
+    }
+
+    public function add($data) {
+        $sql = "INSERT INTO lista (nome, telefone) values (:nome, :telefone)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(":nome", $data['nome']);
+        $stmt->bindValue(":telefone", $data['telefone']);
+        $stmt->execute();
+    }
+
+    public function getContato($id) {
+        $result = array();
+        
+        $sql = "SELECT * FROM lista WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+            $result = $stmt->fetch();
+        }
+        return $result;
+    }
+    
+    public function update($data, $id) {
+        $sql = "UPDATE lista SET nome = :nome, telefone = :telefone WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':nome', $data['nome']);
+        $stmt->bindValue(':telefone', $data['telefone']);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+    }
+
+}
---
+++ slimFrameworks/src/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+// Routes
+
+$app->get('/', function($request, $response, $args) {
+    $lista = new Lista($this->db);
+    $args['lista'] = $lista->getLista();
+
+    return $this->renderer->render($response, 'home.phtml', $args);
+});
+
+$app->get('/add', function($request, $response, $args) {
+    return $this->renderer->render($response, 'add.phtml', $args);
+});
+
+$app->post('/add', function($request, $response, $args) {
+    $data = $request->getParsedBody();
+    $lista = new Lista($this->db);
+    $lista->add($data);
+
+    return $response->withStatus(302)->withHeader("Location", "./");
+});
+
+$app->get('/edit/{id}', function ($request, $response, $args) {
+    $lista = new Lista($this->db);
+    $args['info'] = $lista->getContato($args['id']);
+
+    return $this->renderer->render($response, 'edit.phtml', $args);
+});
+
+$app->post('/edit/{id}', function($request, $response, $args) {
+    $data = $request->getParsedBody();
+    $lista = new Lista($this->db);
+    $lista->update($data, $args['id']);
+
+    return $response->withStatus(302)->withHeader("Location", "./");
+});
