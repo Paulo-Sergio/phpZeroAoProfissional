42500ab49e3aa5662f89ea8015b3fb4e12084448
paulofranca.contato@gmail.com <Ardamaz45!>
2020-04-12 15:32:21 -0300

    estrutura inicial do modelo MVC para o projeto devsbook

---
+++ devsbookMVC/src/Config.php
@@ -0,0 +1,15 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = '/phpZeroAoProfissional/devsbookMVC/public';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = 'localhost';
+    const DB_DATABASE = 'devsbook_mvc';
+    CONST DB_USER = 'root';
+    const DB_PASS = '';
+
+    const ERROR_CONTROLLER = 'ErrorController';
+    const DEFAULT_ACTION = 'index';
+}
